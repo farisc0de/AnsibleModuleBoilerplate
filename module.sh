91171bf4278529f354ea902cd92c8bdb3007c2a1
Faris AL-Otaibi <76238196+farisc0de@users.noreply.github.com>
2024-06-26 07:02:22 +0300

    Create module.sh

---
+++ module.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source $1
+
+function print_error() {
+    printf '{"failed": %s, "msg": "%s"}' "$1" "$2"
+}
+
+function print_status() {
+    printf '{"changed": %s, "msg": "%s"}' "$1" "$2"
+}
+
+case $action in
+    *)
+        failed=true
+        msg="Invalid action"
+        print_error "$failed" "$msg"
+    ;;
+esac
+
+exit 0
